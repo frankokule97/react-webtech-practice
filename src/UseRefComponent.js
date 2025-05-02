2449acf7132dfb75a3956ba8363f32cf86606410
frankokule97 <franko.kule97@gmail.com>
2025-05-02 11:37:04 +0200

    chore: practice for useEffect and useRef hooks with task example with multiple input fields and buttons that responsively changes border and focus of selected input or button

--- src/UseRefComponent.js
+++ src/UseRefComponent.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import "./App.css";
-import { useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const UseRefComponent = () => {
 
-    const name = useRef(null);
-    const age = useRef(null);
-    const gender = useRef(null);
-    const state = useRef(null);
-    const country = useRef(null);
+    const nameRef = useRef(null);
+    const ageRef = useRef(null);
+    const genderRef = useRef(null);
+    const stateRef = useRef(null);
+    const countryRef = useRef(null);
+    const [button, setButton] = useState("");
+
+    const nameHandler = () => {
+        nameRef.current.focus();
+        setButton("name");
+    }
+
+    const ageHandler = () => {
+        ageRef.current.focus();
+        setButton("age");
+    }
+
+    const genderHandler = () => {
+        genderRef.current.focus();
+        setButton("gender");
+    }
+
+    const stateHandler = () => {
+        stateRef.current.focus();
+        setButton("state");
+    }
+
+    const countryHandler = () => {
+        countryRef.current.focus();
+        setButton("country");
+    }
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, []);
 
     return (
         <div className="useRef-container">
@@ -18,18 +48,18 @@ const UseRefComponent = () => {
                 }}>useRef Exercise</h2>
             </div>
             <div className="inputRef-container">
-                <input className="inputRef" placeholder="Name" type="text" ref={name}/>
-                <input className="inputRef" placeholder="Age" type="text" ref={age}/>
-                <input className="inputRef" placeholder="Gender" type="text" ref={gender}/>
-                <input className="inputRef" placeholder="State" type="text" ref={state}/>
-                <input className="inputRef" placeholder="Country" type="text" ref={country}/>
+                <input className="inputRef" placeholder="Name" type="text" ref={nameRef}/>
+                <input className="inputRef" placeholder="Age" type="text" ref={ageRef}/>
+                <input className="inputRef" placeholder="Gender" type="text" ref={genderRef}/>
+                <input className="inputRef" placeholder="State" type="text" ref={stateRef}/>
+                <input className="inputRef" placeholder="Country" type="text" ref={countryRef}/>
             </div>
             <div className="buttonRef-container">
-                <button className="buttonRef">Name</button>
-                <button className="buttonRef">Age</button>
-                <button className="buttonRef">Gender</button>
-                <button className="buttonRef">State</button>
-                <button className="buttonRef">Country</button>
+                <button onClick={nameHandler} className={button === "name" ? "buttonRef active" : "buttonRef"}>Name</button>
+                <button onClick={ageHandler} className={button === "age" ? "buttonRef active" : "buttonRef"}>Age</button>
+                <button onClick={genderHandler} className={button === "gender" ? "buttonRef active" : "buttonRef"}>Gender</button>
+                <button onClick={stateHandler} className={button === "state" ? "buttonRef active" : "buttonRef"}>State</button>
+                <button onClick={countryHandler} className={button === "country" ? "buttonRef active" : "buttonRef"}>Country</button>
             </div>
         </div>
     )
